76e6c955de92e70a24cc050fdcd6dc33c9a03261
GentilleauF <francois.gentilleau@gmail.com>
2023-12-06 16:40:18 +0100

    Ajout de fonctionalité

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 Vue.createApp({
   data() {
     return {
@@ -11,84 +9,146 @@ Vue.createApp({
       myWin: 0,
       myLoose: 0,
       nbrPartie: 0,
-      endGame: false
+      endGame: false,
+      statGame: [],
+      lifeBarPerso: 'green',
+      lifeBarAdversaire: 'green'
     };
   },
 
   methods: {
-    //Attaque du joueur
+//Attaque du joueur
     attackDuPlayer() {
-      console.log("ok");
-      this.vieAdversaire -= 15;
+      let attack =  Math.floor(Math.random() * (20 - 10) + 10);
+      console.log(attack);
+      this.vieAdversaire -= attack;
+
+      // LOG de suivi d'attaque
+      statsDeLattack = "Vous avez infligé " + attack + " de dégats à l'adversaire";
+      this.statGame.unshift(statsDeLattack);
       this.attackDeLadversaire();
     },
 
-    //Attaque de l'adversaire
-    attackDeLadversaire() {
-      this.viePlayer -= 20;
-      console.log(this.viePlayer, this.vieAdversaire);
-      this.counterRound++;
-      if (this.counterRound % 3 == 0) {
-        console.log("55");
-        return (this.chargeSpecialAttack = false);
-      }
-    },
 
-    //Attaque speciale
+
+//Attaque speciale
     attackSpecial() {
       console.log("attackspecial");
-      this.vieAdversaire -= 30;
+      let attack =  Math.floor(Math.random() * (35 - 25) + 25);
+      this.vieAdversaire -= attack;
+
+      // Réinitialise l'attaque speciale
       this.chargeSpecialAttack = true;
+
+      // LOG de suivi d'attaque
+      statsDeLattack = "Vous avez infligé " + attack + " de dégats à l'adversaire"
+      this.statGame.unshift(statsDeLattack)
       this.attackDeLadversaire();
     },
 
-    //Se soigner
+
+//Se soigner
     gainLife() {
+      let heal =  Math.floor(Math.random() * (40 - 15) + 15);
       if (this.viePlayer <= 185) {
-        this.viePlayer += 30;
+        this.viePlayer += heal;
+
+        //LOG de suivi d'attaque
+        let statsDeLattack = ""; 
+        statsDeLattack = "Vous avez gagné "+ heal + "PDV" ;
+        this.statGame.unshift(statsDeLattack);
         this.attackDeLadversaire();
       }
     },
 
-    // Boutton lancer une nouvelle partie
+
+//Attaque de l'adversaire
+    attackDeLadversaire(attack) {
+      let attackAdversaire =  Math.floor(Math.random() * (25 - 15) + 15);
+
+      this.viePlayer -= attackAdversaire;
+      console.log(this.viePlayer, this.vieAdversaire);
+      this.counterRound++;
+
+      // Log de suivi des attaques
+      let statsDeLattack = ""; 
+      statsDeLattack = "Vous a subi " + attackAdversaire + ' de dégats'
+      this.statGame.unshift(statsDeLattack)
+      
+    
+      //Debloque l'attaque speciale tout les 3 tours
+      if (this.counterRound % 3 == 0) {
+        console.log("55");
+        return (this.chargeSpecialAttack = false);
+      }
+
+
+
+      
+    },
+
+
+// Boutton lancer une nouvelle partie
     nouvellePartie () {
+      //reinitialise les parametres
       this.endGame = false;
       this.gameOver= "";
       this.viePlayer= 200;
       this.vieAdversaire= 200;
       this.counterRound= 0;
+      this.statGame = [],
+      this.lifeBarPerso= 'green',
+      this.lifeBarAdversaire= 'green'
     },
 
+// Boutton abandonner
     giveUp () {
       console.log("clickkkk");
       this.endGame = true;
       this.myLoose++;
       this.nbrPartie++;
+      this.chargeSpecialAttack = true
       this.gameOver = "GAME OVER!!! Vous avez abandonné la partie!!";
     }
   },
 
 
-
   watch: {
-    // FIN DE PARIE si viePlayer ==0
+  // FIN DE PARIE si viePlayer ==0
     viePlayer(value){
       if(value<=0){
-        this.gameOver = "GAME OVER!!! Vous avez perdu en" + this.counterRound + ' tours';
+        this.viePlayer = 0;
+        this.gameOver = "GAME OVER!!! Vous avez perdu en " + this.counterRound + ' tours';
         this.endGame = true;
         this.myLoose++;
         this.nbrPartie++;
+        this.chargeSpecialAttack = true
+      }
+      //Changer la couleur de la barre de vie
+      if (value<=100 && value>=50) {
+        this.lifeBarPerso = 'orange'
+      } else if (value<50 && value>=0) {
+        this.lifeBarPerso = 'red'
       }
     },
 
-    // FIN DE PARIE si vieAdversaire ==0
+
+
+// FIN DE PARIE si vieAdversaire ==0
     vieAdversaire(value){
       if(value<=0){
         this.vieAdversaire = 0;
-        this.gameOver = 'Vous avez gagné en' + this.counterRound + ' tours';
+        this.gameOver = 'Vous avez gagné en ' + this.counterRound + ' tours';
         this.endGame = true;
+        this.chargeSpecialAttack = true
         this.myWin++;
         this.nbrPartie++;
+      }
+      //Changer la couleur de la barre de vie
+      if (value<=100 && value>=50) {
+        this.lifeBarAdversaire = 'orange'
+      } else if (value<50 && value>=0) {
+        this.lifeBarAdversaire = 'red'
       }
     }
   }
